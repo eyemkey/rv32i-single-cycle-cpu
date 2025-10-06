4c80278d6fd95f70b02f2a592f1f959f793a6374
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2025-10-06 15:44:01 -0700

    Updated variable names to be more telling

--- rtl/regfile.v
+++ rtl/regfile.v
@@ -25,13 +25,13 @@ module regfile(
         input wire rst_n, //reset when 0
         input wire write_enable, 
         
-        input wire [31:0] rd_data, 
+        input wire [31:0] rd_wdata, 
         input wire [4:0] rs1_addr, 
         input wire [4:0] rs2_addr, 
         input wire [4:0] rd_addr,
         input wire RegWrite,
-        output wire [31:0] rs1_data, 
-        output wire [31:0] rs2_data 
+        output wire [31:0] rs1_rdata, 
+        output wire [31:0] rs2_rdata 
     );
     
     
@@ -44,12 +44,11 @@ module regfile(
                 regs[i] <= 32'b0;
             end
         end else if (write_enable && (rd_addr != 5'd0)) begin //no writing into r0
-            regs[rd_addr] = rd_data;
+            regs[rd_addr] = rd_wdata;
         end
     end
     
-    assign rs1_data = (rs1_addr == 5'd0) ? 32'b0 : regs[rs1_addr]; 
-    assign rs2_data = (rs2_addr == 5'd0) ? 32'b0 : regs[rs2_addr]; 
-    
+    assign rs1_rdata = (rs1_addr == 5'd0) ? 32'b0 : regs[rs1_addr]; 
+    assign rs2_rdata = (rs2_addr == 5'd0) ? 32'b0 : regs[rs2_addr]; 
     
 endmodule
---
+++ tb/tb_regfile.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/06/2025 03:41:13 PM
+// Design Name: 
+// Module Name: tb_regfile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_regfile(
+
+    );
+endmodule
