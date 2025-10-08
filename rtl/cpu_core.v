354889af7d676f5ecd6c83c749d2c05154365d58
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2025-10-07 22:13:20 -0700

    Correctly Integrated dmem.v, allowing storing and loading words

--- rtl/cpu_core.v
+++ rtl/cpu_core.v
@@ -29,6 +29,33 @@ module cpu_core(
     wire [31:0] pc_plus4 = pc_cur + 32'd4; 
     wire [31:0] pc_next = pc_plus4; 
     
+    wire [31:0] instr; 
+    
+    wire [4:0]  rd_addr, rs1_addr, rs2_addr; 
+    wire [2:0]  funct3; 
+    wire [6:0]  funct7; 
+    wire        RegWrite; 
+    wire [3:0]  ALUOp;
+    wire        alu_src_ctrl; 
+    wire        mem_read;
+    wire        mem_write; 
+    wire        mem_to_reg; 
+    wire        illegal;
+    
+    
+    wire [31:0] wb_mux_result; 
+    wire [31:0] rs1_data; 
+    wire [31:0] rs2_data; 
+    
+    wire [31:0] imm_out; 
+    
+    wire [31:0] rdata;
+    
+    wire [31:0] alu_src_1; 
+    wire [31:0] alu_src_2; 
+    wire [31:0] alu_res;
+    wire        zero; 
+    
     pc u_pc(
         .clk(clk), 
         .rst_n(rst_n), 
@@ -36,77 +63,65 @@ module cpu_core(
         .pc(pc_cur)
     );
     
-    
-    wire [31:0] instr; 
     imem u_imem(
         .pc(pc_cur), 
         .instr(instr)
-    ); 
-    
-    
-    wire [4:0] rd, rs1, rs2; 
-    wire [2:0] funct3; 
-    wire [6:0] funct7; 
-    wire RegWrite; 
-    wire [3:0] ALUOp; 
-    wire illegal; 
-    wire alu_src_ctrl; 
+    );
     
     decoder u_decoder(
         .instr(instr), 
-        .rs1(rs1), 
-        .rs2(rs2),  
-        .rd(rd), 
+        .rs1(rs1_addr), 
+        .rs2(rs2_addr),  
+        .rd(rd_addr), 
         .funct3(funct3), 
         .funct7(funct7), 
         .RegWrite(RegWrite), 
         .ALUOp(ALUOp), 
         .alu_src_ctrl(alu_src_ctrl), 
+        .mem_read(mem_read), 
+        .mem_write(mem_write), 
+        .mem_to_reg(mem_to_reg),
         .illegal(illegal)
-    ); 
+    );
     
-    
-    wire [31:0] rd_data; 
-    wire [31:0] rs1_data, rs2_data;
-    wire we; 
+    assign wb_mux_result = mem_to_reg ? rdata : alu_res;
     
     regfile u_regfile(
         .clk(clk), 
         .rst_n(rst_n), 
         .write_enable(RegWrite), 
-        .rd_wdata(rd_data), 
-        .rs1_addr(rs1), 
-        .rs2_addr(rs2), 
-        .rd_addr(rd), 
+        .rd_wdata(wb_mux_result), 
+        .rs1_addr(rs1_addr), 
+        .rs2_addr(rs2_addr), 
+        .rd_addr(rd_addr), 
         .rs1_rdata(rs1_data), 
         .rs2_rdata(rs2_data)
     );
     
-    wire [31:0] imm_out; 
+    
     immgen u_immgen(
         .instr(instr), 
         .imm_out(imm_out)
     ); 
     
-    wire [31:0] alu_src_2; 
-    reg [31:0] alu_src_2_r; 
+    dmem u_dmem(
+        .clk(clk), 
+        .rst_n(rst_n), 
+        .mem_read(mem_read), 
+        .mem_write(mem_write), 
+        .addr(alu_res), 
+        .wdata(rs2_data), 
+        .rdata(rdata)
+    ); 
     
-    always @(*) begin
-        if(alu_src_ctrl == 1'b0) begin
-            alu_src_2_r = rs2_data; 
-        end else begin
-            alu_src_2_r = imm_out;  
-        end
-    end
+    assign alu_src_1 = rs1_data; 
+    assign alu_src_2 = (alu_src_ctrl) ? imm_out : rs2_data;
     
-    assign alu_src_2 = alu_src_2_r; 
-    
-    wire zero; 
     alu u_alu(
-        .a(rs1_data), 
+        .a(alu_src_1), 
         .b(alu_src_2), 
         .op(ALUOp), 
-        .y(rd_data), 
+        .y(alu_res), 
         .zero(zero)
     ); 
     
